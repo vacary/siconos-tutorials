355278a224fd98e110c2cc7aa0e629024fa49fd4
Vincent ACARY <vincent.acary@inria.fr>
2022-03-14 08:56:52 +0100

    [examples] update post.py script

--- examples/mechanics/FEM/ImpactingEulerBernouilliBeam/post.py
+++ examples/mechanics/FEM/ImpactingEulerBernouilliBeam/post.py
@@ -2,12 +2,23 @@ import numpy as np
 
 res= np.loadtxt('percussion.txt')
 #res= np.loadtxt('percussion.txt.lumped')
-
+res= np.loadtxt('percussion_50000.txt')
 #print('res', res)
 
 
 h_list = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12, 1e-13]
-n_list = [10, 50, 100, 500, 1000, 5000, 10000]
+n_list = [10, 50, 100, 500, 1000, 5000, 10000, 50000]
+
+h_list = list(set(res[:,1].tolist()))
+h_list.sort(reverse=True)
+print('h_list', h_list)
+
+n_list = list(set((res[:,0]).astype(int).tolist()))
+n_list.sort()
+print('n_list', n_list)
+
+
+
 
 
 
@@ -15,10 +26,10 @@ n_h = len(h_list)
 n_el = len(n_list)
 
 
-
 import matplotlib.pyplot as plt
 
-print(res[0:4, 0],res[0:4, 2])
+
+############# Convergence in h
 
 plt.subplot(211)
 cnt = 0
@@ -55,9 +66,9 @@ plt.subplot(211)
 cnt = 0
 for n in n_list:
     l = 'n = '+ str(n)
-    print('refl value', res[-1,3])
-    
-    plt.plot(res[cnt*n_h:(cnt+1)*n_h-1, 1], abs(res[cnt*n_h:(cnt+1)*n_h-1, 3]-res[-1,3]), marker='+', label= l )
+    print('ref value for n ', res[(cnt+1)*n_h-1,0], ' and h ', res[(cnt+1)*n_h-1,1], ':', res[(cnt+1)*n_h-1,3])
+    print(res[cnt*n_h:(cnt+1)*n_h-1, 3])
+    plt.plot(res[cnt*n_h:(cnt+1)*n_h-1, 1], abs((res[cnt*n_h:(cnt+1)*n_h-1, 3]-res[(cnt+1)*n_h-1,3]))/abs((res[(cnt+1)*n_h-1,3])), marker='+', label= l )
     cnt =cnt+1
 plt.xlabel('h')
 plt.ylabel('First percussion')
@@ -73,8 +84,9 @@ plt.subplot(212)
 cnt = 0
 for n in n_list:
     l = 'n = '+ str(n)
-    print('refl value', res[-1,4])
-    plt.plot(res[cnt*n_h:(cnt+1)*n_h-1, 1], abs(res[cnt*n_h:(cnt+1)*n_h-1, 4]-res[-1,4]), marker='+', label= l )
+    print('ref value for n ', res[(cnt+1)*n_h-1,0], ' and h ', res[(cnt+1)*n_h-1,1], ':', res[(cnt+1)*n_h-1,4])
+    print(res[cnt*n_h:(cnt+1)*n_h-1, 4])
+    plt.plot(res[cnt*n_h:(cnt+1)*n_h-1, 1], abs((res[cnt*n_h:(cnt+1)*n_h-1, 4]-res[(cnt+1)*n_h-1,4])/abs(res[(cnt+1)*n_h-1,4])), marker='+', label= l )
     cnt =cnt+1
 plt.xlabel('h')
 plt.ylabel('W(0,0)')
@@ -84,7 +96,10 @@ plt.legend()
 plt.grid()
 plt.title('convergence W(0,0) with respect to h')
 
+############# Convergence in n
 
+
+print('\n ############# Convergence in n \n ')
 # swap result
 
 res_ori =res.copy()
@@ -117,13 +132,32 @@ for h in h_list:
     #print(res[cnt*n_h:(cnt+1)*n_h, 1], res[cnt*n_h:(cnt+1)*n_h, 3])
     plt.plot(res[cnt*n_el:(cnt+1)*n_el, 1], res[cnt*n_el:(cnt+1)*n_el, 3], marker='+', label= l )
     cnt =cnt+1
-plt.xlabel('h')
+plt.xlabel('n_el')
 plt.ylabel('First percussion')
 plt.yscale("log")
 plt.xscale("log")
 plt.legend()
 plt.grid()
-plt.title('percussion with respect to h')
+plt.title('percussion with respect to n')
+
+
+plt.subplot(212)
+cnt = 0
+for h in h_list:
+    l = 'h = '+ str(h)
+    #print(res[cnt*n_h:(cnt+1)*n_h, 1], res[cnt*n_h:(cnt+1)*n_h, 3])
+    plt.plot(res[cnt*n_el:(cnt+1)*n_el, 1], res[cnt*n_el:(cnt+1)*n_el, 3], marker='+', label= l )
+    cnt =cnt+1
+plt.xlabel('n_el')
+plt.ylabel('W(0,0)')
+plt.yscale("log")
+plt.xscale("log")
+plt.legend()
+plt.grid()
+plt.title('W(0,0) with respect to n')
+
+
+
 
 plt.figure()
 
@@ -132,17 +166,36 @@ cnt = 0
 for h in h_list:
     l = 'h = '+ str(h)
     #print(res[cnt*n_h:(cnt+1)*n_h, 1], res[cnt*n_h:(cnt+1)*n_h, 3])
-    print('ref value', res[-1,3])
+    print('ref value for h ', res[(cnt+1)*n_el-1,0], ' and n ', res[(cnt+1)*n_el-1,1], ':', res[(cnt+1)*n_el-1,3])
     print(abs(res[cnt*n_el:(cnt+1)*n_el-1, 3]-res[(cnt+1)*n_el-1,3]))
-    plt.plot(res[cnt*n_el:(cnt+1)*n_el-1, 1], abs(res[cnt*n_el:(cnt+1)*n_el-1, 3]-res[(cnt+1)*n_el-1,3]), marker='+', label= l )
+    print(res[cnt*n_el:(cnt+1)*n_el, :])
+    print(res[cnt*n_el:(cnt+1)*n_el-1, 3]-res[(cnt+1)*n_el-1,3])
+    plt.plot(res[cnt*n_el:(cnt+1)*n_el-1, 1], abs(res[cnt*n_el:(cnt+1)*n_el-1, 3]-res[(cnt+1)*n_el-1,3])/abs(res[(cnt+1)*n_el-1,3]), marker='+', label= l )
     cnt =cnt+1
-plt.xlabel('h')
+plt.xlabel('n_el')
 plt.ylabel('Convergence First percussion')
 plt.yscale("log")
 plt.xscale("log")
 plt.legend()
 plt.grid()
-plt.title('percussion with respect to h')
+plt.title('convergence percussion with respect to n')
+
+# plt.subplot(212)
+# cnt = 0
+# for h in h_list:
+#     l = 'h = '+ str(h)
+#     #print(res[cnt*n_h:(cnt+1)*n_h, 1], res[cnt*n_h:(cnt+1)*n_h, 3])
+#     print('ref value for h ', res[(cnt+1)*n_el-1,0], ' and h ', res[(cnt+1)*n_el-1,1], ':', res[(cnt+1)*n_el-1,4])
+#     print(abs(res[cnt*n_el:(cnt+1)*n_el-1, 4]-res[(cnt+1)*n_el-1,4]))
+#     plt.plot(res[cnt*n_el:(cnt+1)*n_el-1, 1], abs(res[cnt*n_el:(cnt+1)*n_el-1, 4]-res[(cnt+1)*n_el-1,4]), marker='+', label= l )
+#     cnt =cnt+1
+# plt.xlabel('n_el')
+# plt.ylabel('Convergence First percussion')
+# plt.yscale("log")
+# plt.xscale("log")
+# plt.legend()
+# plt.grid()
+# plt.title('convergence W(0,0) with respect to n')
 
 
 plt.show()
