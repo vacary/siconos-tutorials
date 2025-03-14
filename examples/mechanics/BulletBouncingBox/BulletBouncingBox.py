3ec20cf18ead24ac2b5262b3a604bf7bccf7d688
Sam <samuel.heidmann@gmail.com>
2025-03-14 15:18:46 +0100

    Fix BulletBouncingBox python example to comply with new interface

--- examples/mechanics/BulletBouncingBox/BulletBouncingBox.py
+++ examples/mechanics/BulletBouncingBox/BulletBouncingBox.py
@@ -24,32 +24,39 @@ except:
     do_plot = False
 if do_plot:
     import os, sys
-    if sys.platform=='linux' and (not 'DISPLAY' in os.environ
-                                  or len(os.environ['DISPLAY'])==0):
-        matplotlib.use('Agg')
-    from matplotlib.pyplot import \
-        subplot, title, plot, grid, show, savefig, ylim
 
-from siconos.kernel import \
-    NonSmoothDynamicalSystem, MoreauJeanOSI, TimeDiscretisation, \
-    FrictionContact, NewtonImpactFrictionNSL, TimeStepping
+    if sys.platform == "linux" and (
+        not "DISPLAY" in os.environ or len(os.environ["DISPLAY"]) == 0
+    ):
+        matplotlib.use("Agg")
+    from matplotlib.pyplot import subplot, title, plot, grid, show, savefig, ylim
 
-import siconos.kernel as sk
+from siconos.modeling import NewtonImpactFrictionNSL, NonSmoothDynamicalSystem
+from siconos.integrators import MoreauJeanOSI
+from siconos.nonsmooth_formulations import FrictionContact
+from siconos.simulation import TimeStepping, TimeDiscretisation
+import siconos.numerics as sn
 
-from siconos.mechanics.collision.bullet import \
-     SiconosBulletCollisionManager
 
-from siconos.mechanics.collision import \
-    SiconosBox, SiconosPlane, RigidBodyDS, SiconosContactor, SiconosContactorSet
+from siconos.mechanics.collision.bullet import SiconosBulletCollisionManager
+
+from siconos.mechanics.collision import (
+    SiconosBox,
+    SiconosPlane,
+    RigidBodyDS,
+    SiconosContactor,
+    SiconosContactorSet,
+)
 
 from numpy import zeros
 from numpy.linalg import norm
+import numpy as np
 
-t0 = 0       # start time
-T = 20       # end time
-h = 0.005    # time step
+t0 = 0  # start time
+T = 20  # end time
+h = 0.005  # time step
 
-g = 9.81     # gravity
+g = 9.81  # gravity
 
 theta = 0.5  # theta scheme
 
@@ -59,20 +66,23 @@ theta = 0.5  # theta scheme
 position_init = 10
 velocity_init = 0
 
+initial_position = np.array([0, 0, position_init, 1, 0, 0, 0], dtype=np.float64)
+initial_velocity = np.array([0, 0, velocity_init, 0, 0, 0], dtype=np.float64)
+inertia = np.eye(3, dtype=np.float64, order="F")
+
 # a box shape
 box1 = SiconosBox(1.0, 1.0, 1.0)
 
 # A Bullet Dynamical System : a shape + a mass (1.0) + position and velocity
-body = RigidBodyDS([0, 0, position_init, 1., 0, 0, 0],
-              [0, 0, velocity_init, 0., 0., 0.],
-              1.0)
+body = RigidBodyDS(initial_position, initial_velocity, 1.0, inertia)
 
 # Add the shape, wrapped in a SiconosContactor, to the body's
 # contactor set.
-body.contactors().push_back(SiconosContactor(box1))
+contactor = SiconosContactor(box1)
+body.contactors().append(contactor)
 
 # set external forces
-weight = [0, 0, -body.scalarMass() * g]
+weight = np.array([0, 0, -body.scalarMass * g], dtype=np.float64)
 body.setConstantFext(weight)
 
 #
@@ -91,7 +101,7 @@ bouncingBox.insertDynamicalSystem(body)
 osi = MoreauJeanOSI(theta)
 
 ground = SiconosPlane()
-groundOffset = [0,0,-0.5,1,0,0,0]
+groundOffset = np.array([0, 0, -0.5, 1, 0, 0, 0], dtype=np.float64)
 
 # (2) Time discretisation --
 timedisc = TimeDiscretisation(t0, h)
@@ -102,7 +112,7 @@ osnspb = FrictionContact(3)
 osnspb.numericsSolverOptions().iparam[0] = 1000
 osnspb.numericsSolverOptions().dparam[0] = 1e-5
 osnspb.setMaxSize(16384)
-osnspb.setMStorageType(1)
+osnspb.setMStorageType(sn.params.NM_SPARSE_BLOCK)
 osnspb.setNumericsVerboseMode(False)
 
 # keep previous solution
@@ -110,7 +120,7 @@ osnspb.setKeepLambdaAndYState(True)
 
 
 # (4) non smooth law
-nslaw = NewtonImpactFrictionNSL(0.8, 0., 0., 3)
+nslaw = NewtonImpactFrictionNSL(0.8, 0.0, 0.0, 3)
 
 # (5) broadphase contact detection
 broadphase = SiconosBulletCollisionManager()
@@ -136,7 +146,7 @@ simulation.insertNonSmoothProblem(osnspb)
 # ->saved in a matrix dataPlot
 
 N = int((T - t0) / h)
-dataPlot = zeros((N+1, 4))
+dataPlot = zeros((N + 1, 4))
 
 #
 # numpy pointers on dense Siconos vectors
@@ -154,7 +164,7 @@ dataPlot[0, 2] = v[2]
 k = 1
 
 # time loop
-while(simulation.hasNextEvent()):
+while simulation.hasNextEvent():
 
     simulation.computeOneStep()
 
@@ -162,16 +172,20 @@ while(simulation.hasNextEvent()):
     dataPlot[k, 1] = q[2]
     dataPlot[k, 2] = v[2]
 
-    #if (broadphase.collisionWorld().getDispatcher().getNumManifolds() > 0):
-    if (broadphase.statistics().new_interactions_created +
-        broadphase.statistics().existing_interactions_processed) > 0:
-        if bouncingBox.topology().\
-          numberOfIndexSet() == 2:
+    # if (broadphase.collisionWorld().getDispatcher().getNumManifolds() > 0):
+    if (
+        broadphase.statistics().new_interactions_created
+        + broadphase.statistics().existing_interactions_processed
+    ) > 0:
+        if bouncingBox.topology().numberOfIndexSet() == 2:
             index1 = sk.interactions(simulation.indexSet(1))
-            if (len(index1) == 4):
-                dataPlot[k, 3] = norm(index1[0].lambda_(1)) + \
-                norm(index1[1].lambda_(1)) + norm(index1[2].lambda_(1)) + \
-                norm(index1[3].lambda_(1))
+            if len(index1) == 4:
+                dataPlot[k, 3] = (
+                    norm(index1[0].lambda_(1))
+                    + norm(index1[1].lambda_(1))
+                    + norm(index1[2].lambda_(1))
+                    + norm(index1[3].lambda_(1))
+                )
 
     k += 1
     simulation.nextStep()
@@ -179,13 +193,13 @@ while(simulation.hasNextEvent()):
 #
 # comparison with the reference file
 #
-from siconos.kernel import SiconosMatrix, getMatrix
+from siconos.input import readMatrixFromFile
 from numpy.linalg import norm
 
-ref = getMatrix(SiconosMatrix("result.ref"))
+ref = readMatrixFromFile("result.ref")
 
 print("norm(dataPlot - ref) = {0}".format(norm(dataPlot - ref)))
-if (norm(dataPlot - ref) > 1e-11):
+if norm(dataPlot - ref) > 1e-11:
     print("Warning. The result is rather different from the reference file.")
 
 
@@ -195,14 +209,14 @@ if (norm(dataPlot - ref) > 1e-11):
 
 if do_plot:
     subplot(511)
-    title('position')
+    title("position")
     plot(dataPlot[0:k, 0], dataPlot[0:k, 1])
     y = ylim()
     plot(ref[0:k, 0], ref[0:k, 1])
     ylim(y)
     grid()
     subplot(513)
-    title('velocity')
+    title("velocity")
     plot(dataPlot[0:k, 0], dataPlot[0:k, 2])
     y = ylim()
     plot(ref[0:k, 0], ref[0:k, 2])
@@ -213,7 +227,7 @@ if do_plot:
     y = ylim()
     plot(ref[0:k, 0], ref[0:k, 3])
     ylim(y)
-    title('lambda')
+    title("lambda")
     grid()
-    savefig('result.png')
+    savefig("result.png")
     show()
