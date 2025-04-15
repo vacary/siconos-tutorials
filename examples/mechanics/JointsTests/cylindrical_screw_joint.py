9600ef28442c00986df0d688cc2ccb08be9aac5a
Sam <samuel.heidmann@gmail.com>
2025-04-15 14:47:14 +0200

    fix test

--- examples/mechanics/JointsTests/cylindrical_screw_joint.py
+++ examples/mechanics/JointsTests/cylindrical_screw_joint.py
@@ -3,13 +3,21 @@ from siconos.mechanics.collision.tools import Contactor
 from siconos.io.mechanics_run import MechanicsHdf5Runner
 
 import siconos.numerics as sn
-import siconos.kernel as sk
+from siconos.simulation import TimeSteppingDirectProjection
+from siconos.integrators import MoreauJeanDirectProjectionOSI
+
+# import time
+
+# print("Script démarré, en attente...")
+# time.sleep(10)
+
 # A demonstration of how to couple the two free axes of a
 # CylindricalJointR in order to construct a screw relation. (Coupled
 # rotational and translational motion.)
 
 # Creation of the hdf5 file for input/output
 with MechanicsHdf5Runner() as io:
+    print('Creating the hdf5 file')
 
     # Bouncy contact with the ground
     io.add_Newton_impact_friction_nsl('contact', mu=0.3, e=0.6)
@@ -44,6 +52,6 @@ with MechanicsHdf5Runner(mode='r+') as io:
            projection_itermax=3,
            projection_tolerance=1e-5,
            projection_tolerance_unilateral=1e-5,
-           time_stepping=sk.TimeSteppingDirectProjection,
-           osi=sk.MoreauJeanDirectProjectionOSI,
+           time_stepping=TimeSteppingDirectProjection,
+           osi=MoreauJeanDirectProjectionOSI,
     )
