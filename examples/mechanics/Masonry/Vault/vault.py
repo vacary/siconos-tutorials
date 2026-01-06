7f9deba2ebde2679da52ebe53e4acf29024ab1d8
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2026-01-06 15:58:19 +0100

    Force CI for examples and push to 'new' cdash (irisa)

--- examples/mechanics/Masonry/Vault/vault.py
+++ examples/mechanics/Masonry/Vault/vault.py
@@ -120,7 +120,7 @@ h_step = 5e-3
 options = sn.solver_options_create(sn.solver_ids.SICONOS_FRICTION_3D_NSGS)
 options.iparam[sn.params.SICONOS_IPARAM_MAX_ITER] = 1000
 options.dparam[sn.params.SICONOS_DPARAM_TOL] = 1e-4
-options.iparam[sn.solver_ids.SICONOS_FRICTION_3D_NSGS_FREEZING_CONTACT] = 100
+options.iparam[sn.params.SICONOS_FRICTION_3D_NSGS_FREEZING_CONTACT] = 100
 
 # Load and run the simulation
 with MechanicsHdf5Runner(mode='r+') as io:
